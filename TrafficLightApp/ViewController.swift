a320510cbd16064c91eef814af128482f5fd097f
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-17 01:55:57 +0300

    Stack view to lights added

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var redLightView: UIView!
-    @IBOutlet var yellowLight: UIView!
-    @IBOutlet var greenLight: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         redLightView.layer.cornerRadius = 50
-        yellowLight.layer.cornerRadius = 50
-        greenLight.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
     }
 }
 
